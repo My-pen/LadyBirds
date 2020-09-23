167e00de96cd9aabaa74ec7c4deb072f64462823
My-pen <60969454+My-pen@users.noreply.github.com>
2020-09-23 20:37:33 +0900

    Create config.js

---
+++ main/config.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCgu8GtqMVuLuJvXd3rVKZkoASJ3QEIDI8",
+  authDomain: "ladybirds-7d802.firebaseapp.com",
+  databaseURL: "https://ladybirds-7d802.firebaseio.com",
+  projectId: "ladybirds-7d802",
+  storageBucket: "ladybirds-7d802.appspot.com",
+  messagingSenderId: "27084868050",
+  appId: "1:27084868050:web:e419912f985a9f9da1200b",
+  measurementId: "G-4L26Q3P406"
+};
