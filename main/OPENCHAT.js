65d4b58d2a8d44b0d39382a00d4d5d9d7aa8db14
My-pen <60969454+My-pen@users.noreply.github.com>
2020-09-25 20:31:57 +0900

    Update OPENCHAT.js

--- main/OPENCHAT.js
+++ main/OPENCHAT.js
@@ -1,13 +1,21 @@
 firebase.auth().onAuthStateChanged(function(user) { 
     if (user) { 
     // User is signed in.
-          logout.classList.remove("hide");
+       LOGOUT.classList.remove("hide");
+       logout.addEventListener("click", ()=>{
+       firebase.auth().signOut().then(()=>{
+       console.log("ログアウトしました");
+        })
+        .catch( (error)=>{
+          console.log(`ログアウト時にエラーが発生しました (${error})`);
+        });
+      });
     } 
       
       else { 
     // No user is signed in.
         showMessage('Not Login', 'ログインしていません');
-        logout.classList.add("hide");
+        LOGOUT.classList.add("hide");
         return(false);
     } 
 }); 
