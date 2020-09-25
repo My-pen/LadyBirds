73d969c646fc2e85e4e5a4c7bbca36550330b4f1
My-pen <60969454+My-pen@users.noreply.github.com>
2020-09-25 20:37:21 +0900

    Update OPENCHAT.js

--- main/OPENCHAT.js
+++ main/OPENCHAT.js
@@ -1,21 +1,31 @@
-firebase.auth().onAuthStateChanged(function(user) { 
-    if (user) { 
-    // User is signed in.
-       LOGOUT.classList.remove("hide");
-       logout.addEventListener("click", ()=>{
-       firebase.auth().signOut().then(()=>{
-       console.log("ログアウトしました");
+    firebase.auth().onAuthStateChanged( (user) => {
+      let LOGOUT = document.getElementById("LOGOUT");
+
+      //-----------------------------------
+      // ログインチェック
+      //-----------------------------------
+      if(! user) {
+        showMessage('Not Login', 'ログインしていません');
+        LOGOUT.classList.add("hide");
+        return(false);
+      }
+
+      //-----------------------------------
+      // ログイン者への処理
+      //-----------------------------------
+      // ログインメッセージ
+      showMessage('Login Complete!', `${user.displayName}さんがログインしました`);
+
+      // ログアウトボタンを表示
+      LOGOUT.classList.remove("hide");
+
+      // ログアウトボタンを押下
+      LOGOUT.addEventListener("click", ()=>{
+        firebase.auth().signOut().then(()=>{
+          console.log("ログアウトしました");
         })
         .catch( (error)=>{
           console.log(`ログアウト時にエラーが発生しました (${error})`);
         });
       });
-    } 
-      
-      else { 
-    // No user is signed in.
-        showMessage('Not Login', 'ログインしていません');
-        LOGOUT.classList.add("hide");
-        return(false);
-    } 
-}); 
+    });
